3d94ef211b2299f57d50d59648090f2346914988
hcvazquez <hernan.c.vazquez@gmail.com>
2019-05-27 01:49:45 -0300

    left-rotation

---
+++ lists/left-rotation.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the rotLeft function below.
+def rotLeft(a, d):
+    result = a.copy()
+    for i in range(len(a)):
+        result[i] = a[(i+d)%(len(a))]
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = rotLeft(a, d)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
